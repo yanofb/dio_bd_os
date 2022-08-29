06a5728c843355ea2e2d476931bfb892fedcb0d0
yanofb <73286248+yanofb@users.noreply.github.com>
2022-08-29 11:28:50 -0300

    Update Untitled_mysql_create.sql

--- Untitled_mysql_create.sql
+++ Untitled_mysql_create.sql
@@ -3,18 +3,18 @@ CREATE TABLE `OrdemServico` (
 	`id_Cliente` INT(45) NOT NULL AUTO_INCREMENT,
 	`Equipe` varchar(45) NOT NULL AUTO_INCREMENT,
 	`Data_Emissao` DATETIME(08) NOT NULL AUTO_INCREMENT,
-	`Tipo de serviço` varchar(45) NOT NULL AUTO_INCREMENT,
-	`Valor Peças` varchar(45) NOT NULL AUTO_INCREMENT,
+	`Tipo de servico` varchar(45) NOT NULL AUTO_INCREMENT,
+	`Valor Pecas` varchar(45) NOT NULL AUTO_INCREMENT,
 	`Valor MO` varchar(45) NOT NULL AUTO_INCREMENT,
 	`Status` varchar(45) NOT NULL AUTO_INCREMENT,
-	`Data_Conclusão` DATETIME(08) NOT NULL AUTO_INCREMENT,
+	`Data_Conclusao` DATETIME(08) NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (`id_OS`)
 );
 
 CREATE TABLE `Equipe` (
 	`id_Funcionario` INT NOT NULL AUTO_INCREMENT,
 	`Nome` varchar(45) NOT NULL AUTO_INCREMENT,
-	`Endereço` varchar(45) NOT NULL AUTO_INCREMENT,
+	`Endereco` varchar(45) NOT NULL AUTO_INCREMENT,
 	`Especialidade` varchar(45) NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (`id_Funcionario`)
 );
@@ -23,35 +23,35 @@ CREATE TABLE `Cliente` (
 	`id_Cliente` INT NOT NULL AUTO_INCREMENT,
 	`Nome` varchar(45) NOT NULL AUTO_INCREMENT,
 	`Endereço` varchar(45) NOT NULL AUTO_INCREMENT,
-	`Tipo de serviço` varchar(45) NOT NULL AUTO_INCREMENT,
+	`Tipo de servico` varchar(45) NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (`id_Cliente`)
 );
 
 CREATE TABLE `MaodeObra` (
 	`id_MO` INT(45) NOT NULL AUTO_INCREMENT,
 	`Valor_MO` varchar(45) NOT NULL AUTO_INCREMENT,
-	`Tipo de serviço` varchar(45) NOT NULL AUTO_INCREMENT,
+	`Tipo de servico` varchar(45) NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (`id_MO`)
 );
 
 CREATE TABLE `Pecas` (
-	`id_Peça` INT NOT NULL AUTO_INCREMENT,
-	`Valor Peça` varchar(45) NOT NULL AUTO_INCREMENT,
-	`Tipo de serviço` varchar(45) NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (`id_Peça`)
+	`id_Peca` INT NOT NULL AUTO_INCREMENT,
+	`Valor Peca` varchar(45) NOT NULL AUTO_INCREMENT,
+	`Tipo de servico` varchar(45) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id_Peca`)
 );
 
 CREATE TABLE `InicioOS` (
 	`id_Clnt` INT NOT NULL,
 	`id_Equipe` INT NOT NULL AUTO_INCREMENT,
-	`Tipo de serviço` varchar(45) NOT NULL AUTO_INCREMENT
+	`Tipo de servico` varchar(45) NOT NULL AUTO_INCREMENT
 );
 
 CREATE TABLE `ClienteAceite` (
 	`id_ClienteAceite` INT NOT NULL AUTO_INCREMENT,
-	`id_PeçaAceite` INT NOT NULL AUTO_INCREMENT,
+	`id_PecaAceite` INT NOT NULL AUTO_INCREMENT,
 	`id_MOAceite` varchar(45) NOT NULL,
-	`Valor Peça` varchar(45) NOT NULL,
+	`Valor Peca` varchar(45) NOT NULL,
 	`Valor_MO` varchar(45) NOT NULL
 );
 
